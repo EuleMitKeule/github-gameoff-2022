81764bb95506106708e7036e9dea3dc8a37a23b5
Lennard Beers <l.beers@outlook.de>
2022-11-06 18:05:46 +0100

    fix(gameplay): prevent projectiles getting stuck in walls

--- WorkingTitle/Assets/WorkingTitle.Unity/Physics/ProjectileComponent.cs
+++ WorkingTitle/Assets/WorkingTitle.Unity/Physics/ProjectileComponent.cs
@@ -43,7 +43,6 @@ namespace WorkingTitle.Unity.Physics
                 var type = assembly.GetType("UnityEditor.LogEntries");
                 var method = type.GetMethod("Clear");
                 method.Invoke(new object(), null);
-            
                 Destroy(gameObject);
             }
         }
@@ -51,39 +50,34 @@ namespace WorkingTitle.Unity.Physics
         void CheckCollision()
         {
             var distance = Rigidbody.velocity.magnitude * Time.fixedDeltaTime;
-            var hits = new RaycastHit2D[1];
-            Collider.Cast(
-                Rigidbody.velocity,
-                ContactFilter,
-                hits,
-                distance);
-
+            var hits = PerformCast(distance);
+            
+            if (hits.Length == 0) return;
+            
             var hit = hits[0];
-            if (!hit) return;
-            
             HandleCollision(hit.collider.gameObject, hit.point, hit.normal);
         }
 
-        void Check2ndCollision()
-        {
-            var distance = Rigidbody.velocity.magnitude * Time.fixedDeltaTime;
-            var hits = new RaycastHit2D[1];
-            var hitCount = Collider.Cast(
-                Rigidbody.velocity,
-                ContactFilter,
-                hits,
-                distance);
-
-            Debug.Log($"hitCount: {hits.Length}");
-            if (hitCount <= 1)
-            {
-                Rigidbody.position += Rigidbody.velocity * Time.fixedDeltaTime;
-                return;
-            }
-            var hit = hits[1];
-            
-            HandleCollision(hit.collider.gameObject, hit.point, hit.normal);
-        }
+        // void Check2ndCollision()
+        // {
+        //     var distance = Rigidbody.velocity.magnitude * Time.fixedDeltaTime;
+        //     var hits = new RaycastHit2D[1];
+        //     var hitCount = Collider.Cast(
+        //         Rigidbody.velocity,
+        //         ContactFilter,
+        //         hits,
+        //         distance);
+        //
+        //     Debug.Log($"hitCount: {hits.Length}");
+        //     if (hitCount <= 1)
+        //     {
+        //         Rigidbody.position += Rigidbody.velocity * Time.fixedDeltaTime;
+        //         return;
+        //     }
+        //     var hit = hits[1];
+        //     
+        //     HandleCollision(hit.collider.gameObject, hit.point, hit.normal);
+        // }
 
         void HandleCollision(GameObject contactObject, Vector2 contactPosition, Vector2 contactNormal)
         {
@@ -107,22 +101,46 @@ namespace WorkingTitle.Unity.Physics
             
             Ricochets -= 1;
 
-            var correctNormalSign = Vector2.Dot(contactNormal, Rigidbody.velocity) > 0 ? -1 : 1;
-            contactNormal *= correctNormalSign;
-            //if (correctNormalSign < 0) Debug.Log("normal sign corrected!");
+            // if (contactNormal == -Rigidbody.velocity.normalized)
+            // {
+            //     Debug.LogWarning("Projectile was inside object");
+            //     Destroy(gameObject);
+            //     return;
+            // }
+            
+            // var correctNormalSign = Vector2.Dot(contactNormal, Rigidbody.velocity) > 0 ? -1 : 1;
+            // contactNormal *= correctNormalSign;
+            // if (correctNormalSign < 0)
+            // {
+            //     Debug.Log("Projectile collided from inside object");
+            // }
 
-            var collisionAngle = Vector2.SignedAngle(-Rigidbody.velocity, contactNormal);
-            var reflectionDirection = Quaternion.Euler(0, 0, collisionAngle) * contactNormal * Rigidbody.velocity.magnitude;
+            var reflectedVelocity = Vector2.Reflect(Rigidbody.velocity, contactNormal);
+            
+            // var collisionAngle = Vector2.SignedAngle(-Rigidbody.velocity, contactNormal);
+            // var reflectionDirection = Quaternion.Euler(0, 0, collisionAngle) * contactNormal * Rigidbody.velocity.magnitude;
             
             //Rigidbody.position = (Vector3)contactPosition + reflectionDirection.normalized * (Rigidbody.velocity.magnitude * Time.fixedDeltaTime);
-            Rigidbody.position = contactPosition;
-            Rigidbody.rotation = Vector2.SignedAngle(Vector2.up, reflectionDirection);
-            Rigidbody.velocity = reflectionDirection;
+            // Rigidbody.position = contactPosition;
+            Rigidbody.rotation = Vector2.SignedAngle(Vector2.up, reflectedVelocity);
+            Rigidbody.velocity = reflectedVelocity;
 
             //Debug.Log($"contactPosition: {contactPosition}\tnormal: {contactNormal}\nposition: {transform.position}\tvelocity: {Rigidbody.velocity}");
             Debug.DrawRay(contactPosition, contactNormal, Color.cyan, 1);
             
-            Check2ndCollision();
+            // Check2ndCollision();
+        }
+
+        RaycastHit2D[] PerformCast(float distance = 0)
+        {
+            var hits = new RaycastHit2D[10];
+            var hitCount = Collider.Cast(
+                Rigidbody.velocity,
+                ContactFilter,
+                hits,
+                distance);
+            Array.Resize(ref hits, hitCount);
+            return hits;
         }
 
 #if UNITY_EDITOR
