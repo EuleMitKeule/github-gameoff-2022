a6dfca67b7aa17201de2dbf65f0713cb4bd4eade
Ben Korinth <58632857+mifiamigahna@users.noreply.github.com>
2022-11-12 22:39:54 +0100

    feat(graphics): add tiles

---
+++ WorkingTitle/Assets/WorkingTitle.Unity/Map/Tiles/CustomTile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace WorkingTitle.Unity.Map.Tiles
+{
+    [CreateAssetMenu(menuName = "Tile/" + nameof(CustomTile), fileName = nameof(CustomTile))]
+    public class CustomTile : Tile
+    {
+        
+    }
+}
