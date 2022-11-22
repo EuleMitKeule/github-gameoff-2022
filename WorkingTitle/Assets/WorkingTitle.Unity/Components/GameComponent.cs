6d1ec338e0facd74c1ada3e8f3313da5b427bfb5
Ben Korinth <ben.korinth@rub.de>
2022-11-23 00:24:00 +0100

    feat(gameplay): implement player death and restart mechanics

--- WorkingTitle/Assets/WorkingTitle.Unity/Components/GameComponent.cs
+++ WorkingTitle/Assets/WorkingTitle.Unity/Components/GameComponent.cs
@@ -1,27 +1,48 @@
-﻿using Sirenix.OdinInspector;
+﻿using System;
+using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using WorkingTitle.Unity.Assets;
+using WorkingTitle.Unity.Components.Health;
 using WorkingTitle.Unity.Components.Map;
 using WorkingTitle.Unity.Components.Pooling;
+using WorkingTitle.Unity.Components.UI;
 
 namespace WorkingTitle.Unity.Components
 {
     public class GameComponent : SerializedMonoBehaviour
     {
-        [OdinSerialize] public GameAsset GameAsset { get; set; }
+        [OdinSerialize]
+        public GameAsset GameAsset { get; set; }
 
-        GameObject MapObject { get; set; }
         public GameObject PlayerObject { get; set; }
-
+        GameObject MapObject { get; set; }
         PoolComponent PoolComponent { get; set; }
+        UiComponent UiComponent { get; set; }        
         
         void Awake()
         {
             PoolComponent = FindObjectOfType<PoolComponent>();
+            UiComponent = GetComponentInChildren<UiComponent>();
             
             MapObject = Instantiate(GameAsset.MapPrefab, transform);
             PlayerObject = PoolComponent.Allocate(GameAsset.PlayerPrefab);
+
+            var playerHealthComponent = PlayerObject.GetComponent<HealthComponent>();
+            playerHealthComponent.Death += OnPlayerDeath;
+        }
+
+        void OnPlayerDeath(object sender, EventArgs e)
+        {
+            Time.timeScale = 0f;
+            UiComponent.ShowGameOver();
+        }
+
+        public void ReloadScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Time.timeScale = 1f;
         }
     }
 }
---
+++ WorkingTitle/Assets/WorkingTitle.Unity/Components/UI/UiComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+using WorkingTitle.Unity.Components.Health;
+
+namespace WorkingTitle.Unity.Components.UI
+{
+    public class UiComponent : SerializedMonoBehaviour
+    {
+        [OdinSerialize]
+        GameObject OverlayGameOver { get; set; }
+
+        public void ShowGameOver()
+        {
+            OverlayGameOver.SetActive(true);
+        }
+
+        public void OnButtonOkClick()
+        {
+            Debug.Log("sed");
+        }
+    }
+}
