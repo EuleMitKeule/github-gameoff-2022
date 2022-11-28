86eb3c815ce60ed4f8663dc89855eb5be5c6b2ef
Ben Korinth <ben.korinth@rub.de>
2022-11-28 16:29:07 +0100

    feat(graphics): implement start menu screen

--- TanksOnAPlain/Assets/TanksOnAPlain.Unity/Components/UI/UiComponent.cs
+++ TanksOnAPlain/Assets/TanksOnAPlain.Unity/Components/UI/UiComponent.cs
@@ -12,22 +12,41 @@ namespace TanksOnAPlain.Unity.Components.UI
         
         [OdinSerialize]
         List<CanvasGroup> CanvasGroupsToHide { get; set; }
+        
+        [OdinSerialize]
+        CanvasGroup CanvasGroupStartScreen { get; set; }
 
         public void ShowGameOver()
         {
             foreach (var canvasGroup in CanvasGroupsToShow)
             {
-                canvasGroup.alpha = 1;
-                canvasGroup.interactable = true;
-                canvasGroup.blocksRaycasts = true;
+                EnableCanvasGroup(canvasGroup);
             }
             
             foreach (var canvasGroup in CanvasGroupsToHide)
             {
-                canvasGroup.alpha = 0;
-                canvasGroup.interactable = false;
-                canvasGroup.blocksRaycasts = false;
+                DisableCanvasGroup(canvasGroup);
             }
+        }
+
+        public void StartGame()
+        {
+            Time.timeScale = 1;
+            DisableCanvasGroup(CanvasGroupStartScreen);
+        }
+        
+        void EnableCanvasGroup(CanvasGroup canvasGroup)
+        {
+            canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        }
+        
+        void DisableCanvasGroup(CanvasGroup canvasGroup)
+        {
+            canvasGroup.alpha = 0;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
         }
     }
 }
