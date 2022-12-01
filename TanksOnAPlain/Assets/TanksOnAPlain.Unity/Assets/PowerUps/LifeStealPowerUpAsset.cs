ca37722fb7a232293e24d25a374607abee7705c5
Lennard Beers <l.beers@outlook.de>
2022-12-01 11:13:14 +0100

    fix: various fixes

--- TanksOnAPlain/Assets/TanksOnAPlain.Unity/Assets/PowerUps/LifeStealPowerUpAsset.cs
+++ TanksOnAPlain/Assets/TanksOnAPlain.Unity/Assets/PowerUps/LifeStealPowerUpAsset.cs
@@ -7,12 +7,8 @@ namespace TanksOnAPlain.Unity.Assets.PowerUps
     [CreateAssetMenu(menuName = "PowerUps/" + nameof(LifeStealPowerUpAsset), fileName = nameof(LifeStealPowerUpAsset))]
     public class LifeStealPowerUpAsset : PowerUpAsset
     {
-        [OdinSerialize]
-        [SuffixLabel("*%")]
-        public float LifeStealPercentage { get; set; }
-        
-        [OdinSerialize]
-        [SuffixLabel("+%")]
-        public float LifeSteal { get; set; }
+        [OdinSerialize] 
+        [SuffixLabel("+")] 
+        public int LifeSteal { get; set; } = 1;
     }
 }
